54e55975666747f614400bca1944f662802adeb1
CS LAB <jiras4507@gmail.com>
2024-07-24 00:03:26 -0700

    dccjio

---
+++ vac.py
@@ -0,0 +1,6 @@
+import statistics
+sample=[130,137,136,142,135]
+var=statistics.pvariance(sample)
+print("the variance is:",var)
+sd=statistics.pstdev(sample)
+print("the standard deviation is:",sd)
